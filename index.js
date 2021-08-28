a1ea8158c28e7654a1a43aa7b4ff1d984a05aa04
Karan Bisht <karanbisht511@gmail.com>
2021-08-28 17:58:29 +0530

    buttons

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
